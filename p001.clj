1c5e87e50c112e3d8207503f8836a6e0c38f1f10
Dustin Hu <DaShingHu@users.noreply.github.com>
2016-12-24 20:17:24 -0500

    Created P001
    Well this taught me more than the three chapters of "Clojure for the Brave and True" did.
    There's a better solution with mod, but I couldn't figure out how to get it to work. This will do for now.

---
+++ p001.clj
@@ -0,0 +1,14 @@
+(defn get-first-thou-mult
+  ([n]
+   (get-first-thou-mult 0 n []))
+
+  ([i n arr]
+   (if (< i 1000)
+     (get-first-thou-mult (+ i n) n (conj arr i))
+     arr)))
+
+(defn euler-one
+  []
+  (reduce +
+   (set
+    (reduce conj (get-first-thou-mult 3) (get-first-thou-mult 5)))))
